f4cc9cfe4d0f1e599aa421511d3a7049ac6d4dc2
Sunil Ghanchi <88071734+sunilghanchi@users.noreply.github.com>
2024-10-15 11:54:02 +0530

    Update download_models.sh

--- builder/download_models.sh
+++ builder/download_models.sh
@@ -25,14 +25,14 @@ download() {
   cp "$cache_path" "$destination_path"
 }
 
-faster_whisper_model_dir="${MODELS_DIR}/faster-whisper-large-v3"
+faster_whisper_model_dir="${MODELS_DIR}/faster-whisper-large-v3-turbo-ct2"
 mkdir -p $faster_whisper_model_dir
 
-download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/config.json" "$faster_whisper_model_dir/config.json"
-download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/model.bin" "$faster_whisper_model_dir/model.bin"
-download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/preprocessor_config.json" "$faster_whisper_model_dir/preprocessor_config.json"
-download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/tokenizer.json" "$faster_whisper_model_dir/tokenizer.json"
-download "https://huggingface.co/Systran/faster-whisper-large-v3/resolve/main/vocabulary.json" "$faster_whisper_model_dir/vocabulary.json"
+download "https://huggingface.co/deepdml/faster-whisper-large-v3-turbo-ct2/resolve/main/config.json" "$faster_whisper_model_dir/config.json"
+download "https://huggingface.co/deepdml/faster-whisper-large-v3-turbo-ct2/resolve/main/model.bin" "$faster_whisper_model_dir/model.bin"
+download "https://huggingface.co/deepdml/faster-whisper-large-v3-turbo-ct2/resolve/main/preprocessor_config.json" "$faster_whisper_model_dir/preprocessor_config.json"
+download "https://huggingface.co/deepdml/faster-whisper-large-v3-turbo-ct2/resolve/main/tokenizer.json" "$faster_whisper_model_dir/tokenizer.json"
+download "https://huggingface.co/deepdml/faster-whisper-large-v3-turbo-ct2/resolve/main/vocabulary.json" "$faster_whisper_model_dir/vocabulary.json"
 
 vad_model_dir="${MODELS_DIR}/vad"
 mkdir -p $vad_model_dir
